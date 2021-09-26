9c036e7e9c83584fe317b821c15ae3ac24e946b4
anhmuoi <anhmuoi200147@gmail.com>
2021-09-26 21:07:51 +0700

    delete-history-folder

--- .history/dev_20210826142857.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream.h>
-int main() {
-    // Write C++ code here
-    std::cout << "Hello world!";
-
-    return 0;
-}
--- .history/dev_20210826142920.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream.h>
-int main() {
-    // Write C++ code here
-   cout << "Hello world!";
-
-    return 0;
-}
